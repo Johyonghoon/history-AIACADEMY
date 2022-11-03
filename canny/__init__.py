f43a5f3e7ef63f18a9e709156cdc71a834db8626
johyonghoon <johyonghoon01@gmail.com>
2022-11-03 18:18:22 +0900

    2022-11-03 final

--- canny/__init__.py
+++ canny/__init__.py
@@ -1,46 +1,32 @@
-from canny.views import LennaController
+from canny.views import MenuController
 from util.menu import Menu
-
-"""
-이미지 읽기의 flag는 3가지가 있습니다.
-cv.IMREAD_COLOR : 이미지 파일을 Color로 읽어들입니다. 
-                  투명한 부분은 무시되며, Default값입니다.
-cv.IMREAD_GRAYSCALE : 이미지를 Grayscale로 읽어 들입니다.
-                      실제 이미지 처리시 중간단계로 많이 사용합니다.
-cv.IMREAD_UNCHANGED : 이미지파일을 alpha channel까지 포함하여 읽어 들입니다
-3개의 flag대신에 1, 0, -1을 사용해도 됩니다.
-Shape is (512, 512, 3)
-x축 : 512 (앞)
-y축 : 512 (뒤)
-3은 RGB로 되어있다.
-cv.waitKey(0) : keyboard입력을 대기하는 함수로 
-                 0이면 key입력까지 무한대기이며.
-                 특정 시간동안 대기하려면 milisecond값을 넣어주면 됩니다.
-
-cv.destroyAllWindows() 화면에 나타난 윈도우를 종료합니다. 
-                    일반적으로 위 3개는 같이 사용됩니다.
-"""
 LENNA = "Lenna.png"
 SOCCER = "https://docs.opencv.org/4.x/roi.jpg"
-BUILDING = "https://www.charlezz.com/wordpress/wp-content/uploads/2021/06/www.charlezz.com-opencv-building.jpg"
+BUILDING="http://amroamroamro.github.io/mexopencv/opencv_contrib/fast_hough_transform_demo_01.png"
+HAAR = "haarcascade_frontalface_alt.xml" # 가중치 파일
+GIRL = "girl.jpg"
+GIRL_INCLINED = "girl_incliend.png"
+GIRL_SIDE_FACE = "girl_side_face.jpg"
+GIRL_WITH_MOM = "girl_with_mom.jpg"
+CAT = "cat.jpg"
+FACE_TARGET = ""
+FACE_OBJECT = ""
 if __name__ == '__main__':
-    api = LennaController()
+    api = MenuController()
     while True:
-        menus = ["종료", "원본보기", "그레이스케일", "엣지검출",
-                 "허프 직선검출"]
+        menus = ["종료","원본보기","그레이스케일",
+                 "엣지검출","직선검출","얼굴인식",
+                 "모자이크","얼굴추출"]
         menu = Menu.menu(menus)
-        if menu == '0':
+
+        if menu == "0":
             api.menu_0(menus[0])
             break
-        elif menu == '1':
-            api.menu_1(menus[1], LENNA)
-        elif menu == '2':
-            api.menu_2(menus[2], SOCCER)
-        elif menu == '3':
-            api.menu_3(menus[3], BUILDING)
-        elif menu == '4':
-            api.menu_4(menus[4], BUILDING)
-
+        elif menu == "1": api.menu_1(menus[1],LENNA)
+        elif menu == "2": api.menu_2(menus[2],SOCCER)
+        elif menu == "3": api.menu_3(menus[3],SOCCER)
+        elif menu == "4": api.menu_4(menus[4],BUILDING)
+        elif menu == "5": api.menu_5(menus[5],HAAR,GIRL)
+        elif menu == "6": api.menu_6(menus[6],CAT)
         else:
-            print("### 해당 메뉴 없음 ###")
-
+            print(" ### 해당 메뉴 없음 ### ")
--- canny/views.py
+++ canny/views.py
@@ -1,55 +1,48 @@
-from io import BytesIO
-
-import numpy as np
-from PIL import Image
 from matplotlib import pyplot as plt
-
-from canny.models import LennaModel, image_read, gray_scale, ImageToNumberArray
-from util.dataset import Dataset
+from canny.services import ImageToNumberArray, ExecuteLambda, Hough, Haar
 import cv2 as cv
-LENNA = "Lenna.png"
-SOCCER = "https://docs.opencv.org/4.x/roi.jpg"
+import numpy as np
+
+from util.dataset import Dataset
 
 
-class LennaController(object):
+class MenuController(object):
 
-    dataset = Dataset()
-    model = LennaModel()
-
-    def __init__(self):
-        pass
-
-    def __str__(self):
-        return f''
+    @staticmethod
+    def menu_0(*params):
+         print(params[0])
 
     @staticmethod
     def menu_1(*params):
         print(params[0])
-        img = image_read(params[1])
+        img = ExecuteLambda('IMAGE_READ_FOR_CV', params[1])
         print(f'cv2 버전 {cv.__version__}')  # cv2 버전 4.6.0
-        print(f'Shape is {img.shape}')
-        cv.imshow('Gray', img)
+        print(f' Shape is {img.shape}')
+        cv.imshow('Original', img)
         cv.waitKey(0)
-        cv.destroyAllwindows()
+        cv.destroyAllWindows()
 
     @staticmethod
     def menu_2(*params):
         print(params[0])
         arr = ImageToNumberArray(params[1])
-        img = (lambda x: x[:, :, 0] * 0.114 + x[:, :, 1] * 0.587 + x[:, :, 2] * 0.229)(arr)
-        plt.imshow((lambda x: Image.fromarray(x))(img))
+        img = ExecuteLambda('GRAY_SCALE', arr)
+        plt.imshow(ExecuteLambda('IMAGE_FROM_ARRAY', img))
         plt.show()
+
 
     @staticmethod
     def menu_3(*params):
         print(params[0])
         ### 디스크에서 읽는 경우 ###
-        # img = cv.imread('./data/messi5.jpg', 0)
-        ### 메모리에서 읽는 경우 BEGIN ###
-        # url = "https://docs.opencv.org/4.x/roi.jpg"
+        # img = cv.imread('./data/roi.jpg', 0)
+        # img = cv.imread(img, 0)
+        ### 메모리에서 읽는 경우 ###
         img = ImageToNumberArray(params[1])
-        print(f'img typ : {type(img)}')
-        edges = cv.Canny(img, 100, 200)
+        print(f'img type : {type(img)}')
+        # img = GaussianBlur(img, 1, 1) cv.Canny() 를 사용하지 않는 경우 필요
+        # img = Canny(img, 50, 150) cv.Canny() 를 사용하지 않는 경우 필요
+        edges = cv.Canny(np.array(img), 100, 200)
         plt.subplot(121), plt.imshow(img, cmap='gray')
         plt.title('Original Image'), plt.xticks([]), plt.yticks([])
         plt.subplot(122), plt.imshow(edges, cmap='gray')
@@ -60,20 +53,45 @@ class LennaController(object):
     def menu_4(*params):
         print(params[0])
         img = ImageToNumberArray(params[1])
-        edges = cv.Canny(img, 100, 200)
-        lines = cv.HoughLinesP(edges, 1, np.pi/180., 120, minLineLength=50, maxLineGap=5)
-        dst = cv.cvtColor(edges, cv.COLOR_GRAY2BGR)
-        if lines is not None:
-            for i in range(lines.shape[0]):
-                pt1 = (lines[i][0][0], lines[i][0][1])
-                pt2 = (lines[i][0][2], lines[i][0][3])
-                cv.line(dst, pt1, pt2, (255, 0, 0), 2, cv.LINE_AA)
+        edges = cv.Canny(img, 100, 200) # (image, threshold 1=100, threshold 2=200)
+        dst = Hough(edges)
         plt.subplot(121), plt.imshow(img, cmap='gray')
         plt.title('Original Image'), plt.xticks([]), plt.yticks([])
         plt.subplot(122), plt.imshow(dst, cmap='gray')
         plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
         plt.show()
 
-    def submit(self):
+    @staticmethod
+    def menu_5(*param):
+        print(param[0])
+        haar = cv.CascadeClassifier(f"{Dataset().context}{param[1]} ")
+        girl = param[2]
+        girl_original = ExecuteLambda('IMAGE_READ_FOR_PLT', girl)
+        girl_gray = ExecuteLambda('GRAY_SCALE', girl_original)
+        girl_canny = cv.Canny(np.array(girl_original), 10, 100)
+        lines = cv.HoughLinesP(girl_canny, 1, np.pi / 180., 120, minLineLength=50, maxLineGap=5)
+        girl_hough = cv.cvtColor(girl_canny, cv.COLOR_GRAY2BGR)
+        girl_haar = haar.detectMultiScale(girl_original, minSize=(150, 150))
+
+        plt.subplot(151), plt.imshow(girl_original, cmap='gray')
+        plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+
+        Haar(girl_haar, girl_hough, girl_original, lines)
+
+        plt.subplot(152), plt.imshow(girl_gray, cmap='gray')
+        plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(153), plt.imshow(girl_canny, cmap='gray')
+        plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(154), plt.imshow(girl_hough, cmap='gray')
+        plt.title('Hough Image'), plt.xticks([]), plt.yticks([])
+        plt.subplot(155), plt.imshow(girl_original, cmap='gray')
+        plt.title('HAAR Image'), plt.xticks([]), plt.yticks([])
+        plt.show()
+
+
+
+
+    @staticmethod
+    def menu_6(*param):
         pass
 
