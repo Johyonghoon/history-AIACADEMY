e7e9c556234cbb20d73a48b6ef8014dd79e14a07
johyonghoon <johyonghoon01@gmail.com>
2022-11-09 15:05:31 +0900

    first commit

---
+++ data_structures/unit_5_skill.py
@@ -0,0 +1,17 @@
+def practice_arithmetic():
+    distance = 12
+    calc = int(0.2467 * distance + 4.159)
+    print('### Practice ###')
+    print(calc)
+
+def judge_arithmetic():
+    ap = 102
+    calc = ap * 0.6 + 225
+    print('### Judge ###')
+    print(calc)
+
+
+if __name__ == '__main__':
+    practice_arithmetic()
+    print('*'*20)
+    judge_arithmetic()
---
+++ data_structures/unit_6_average.py
@@ -0,0 +1,30 @@
+def practice_input_split(a):
+    print('### Practice ###')
+
+
+
+def judge_variable(a):
+    print('### Judge_variable ###')
+
+
+def judge_average(a):
+    print('### Judge_average ###')
+
+
+def unit_6_menu():
+    for i, j in enumerate(ls):
+        
+
+
+if __name__ == '__main__':
+    menu = unit_6_menu()
+    while True:
+        if menu == 0:
+            print("종료")
+
+    practice_input_split()
+    print('*' * 20)
+    judge_variable()
+    print('*' * 20)
+    judge_average()
+
