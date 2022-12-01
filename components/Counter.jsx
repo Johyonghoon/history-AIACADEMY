c096bebe08a3f46758b8bc5764806b30e22c6138
johyonghoon <johyonghoon01@gmail.com>
2022-12-01 17:56:49 +0900

    first

---
+++ components/Counter.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+    return (<>
+        <h1>PlusMinus</h1>
+        <div>결과 값 : {count}</div>
+        <button onClick={() => {setCount(count + 1)}}>
+            +
+        </button>
+        <button onClick={() => {setCount(count - 1)}}>
+            +
+        </button>
+    </>)
+}
+
+export default Counter
