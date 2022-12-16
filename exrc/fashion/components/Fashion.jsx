8bc80bd8738506e05d4cdf5129729c5dd748fef7
johyonghoon <johyonghoon01@gmail.com>
2022-12-16 18:18:49 +0900

    2022-12-16 18:18

--- exrc/fashion/components/Fashion.jsx
+++ exrc/fashion/components/Fashion.jsx
@@ -10,10 +10,10 @@ const Fashion = () => {
       const {value, name} = e.target
       setInputs({...inputs, [name]: value})
     }
-    const getOnClick = e => {
+    const postOnClick = e => {
         e.preventDefault()
         alert(`사용자 이름: ${JSON.stringify(test_num)}`)
-        fashion(test_num)
+        postfashion(test_num)
         .then((res)=>{
             alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
         })
@@ -22,10 +22,10 @@ const Fashion = () => {
             alert('숫자를 다시 입력해주세요.')
         })
     }
-    const postOnClick = e => {
+    const getOnClick = e => {
         e.preventDefault()
         alert(`사용자 이름: ${JSON.stringify(test_num)}`)
-        fashion(test_num)
+        getfashion(test_num)
         .then((res)=>{
             alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
         })
@@ -38,11 +38,12 @@ const Fashion = () => {
     <h1>FASHION</h1>
     <p>카테고리를 알고 싶은 옷의 번호를 입력해주세요.</p>
     <input type="text" placeholder="테스트할 옷 번호" name="test_num" onChange={onChange}/>
-    <form method="get">
-    <button onClick={getOnClick}>GetFashion</button>
-    </form>
+
     <form method="post">
     <button onClick={postOnClick}>PostFashion</button>
+    </form>
+    <form method="get">
+    <button onClick={getOnClick}>GetFashion</button>
     </form>
     </>)
 }
