38f52d9278521392eb2f7d1cd426a2080af8be65
johyonghoon <johyonghoon01@gmail.com>
2022-12-16 18:01:36 +0900

    2022-12-16 final

--- exrc/fashion/api/index.js
+++ exrc/fashion/api/index.js
@@ -1,3 +1,7 @@
 import axios from "axios"
 const server = `http://localhost:8000`
-export const fashion = req => axios.post(`${server}/exrc/fashion`, req)
+export const fashion = test_num => axios(`${server}/exrc/fashion?test_num=${test_num}`)
+
+
+//export const getfashion = test_num => axios.get(`${server}/exrc/fashion?test_num=${test_num}`)
+//export const postfashion = test_num => axios.post(`${server}/exrc/fashion?test_num=${test_num}`, {test_num})
--- exrc/fashion/components/Fashion.jsx
+++ exrc/fashion/components/Fashion.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { fashion } from "../api"
+import { fashion, getfashion, postfashion } from "../api"
 
 const Fashion = () => {
     const [inputs, setInputs] = useState({})
@@ -10,14 +10,23 @@ const Fashion = () => {
       const {value, name} = e.target
       setInputs({...inputs, [name]: value})
     }
-    const onClick = e => {
+    const getOnClick = e => {
         e.preventDefault()
-        const request = {test_num}
-        alert(`사용자 이름: ${JSON.stringify(request)}`)
-        fashion(request)
+        alert(`사용자 이름: ${JSON.stringify(test_num)}`)
+        fashion(test_num)
         .then((res)=>{
-            console.log(`Response is ${res.data.result}`)
-            localStorage.setItem('token', JSON.stringify(res.data.result))
+            alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
+        })
+        .catch((err)=>{
+            console.log(err)
+            alert('숫자를 다시 입력해주세요.')
+        })
+    }
+    const postOnClick = e => {
+        e.preventDefault()
+        alert(`사용자 이름: ${JSON.stringify(test_num)}`)
+        fashion(test_num)
+        .then((res)=>{
             alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
         })
         .catch((err)=>{
@@ -29,9 +38,15 @@ const Fashion = () => {
     <h1>FASHION</h1>
     <p>카테고리를 알고 싶은 옷의 번호를 입력해주세요.</p>
     <input type="text" placeholder="테스트할 옷 번호" name="test_num" onChange={onChange}/>
-
-    <button onClick={onClick}>옷의 카테고리 찾기</button>
+    <form method="get">
+    <button onClick={getOnClick}>GetFashion</button>
+    </form>
+    <form method="post">
+    <button onClick={postOnClick}>PostFashion</button>
+    </form>
     </>)
 }
+
+
 
 export default Fashion
