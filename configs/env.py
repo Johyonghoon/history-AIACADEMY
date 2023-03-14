1a8265fe3ba3caaebfde63caed041f2ab6a15906
Johyonghoon <johyonghoon01@gmail.com>
2023-03-14 10:15:15 +0900

    first commit

---
+++ configs/env.py
@@ -0,0 +1,7 @@
+USERNAME = "admin"
+PASSWORD = "root0704"
+HOSTNAME = "johyonghoon.cdqa9rl4mpfr.ap-northeast-2.rds.amazonaws.com"
+DATABASE = "mydb"
+PORT = 3306
+CHARSET = "utf8"
+DB_URL = f"mysql+pymysql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
---
+++ entities/user.py
@@ -0,0 +1,39 @@
+from .mixins import TimestampMixin
+from configs.database import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
+
+class User(Base, TimestampMixin):
+
+    __tablename__ = "users"
+
+    user_id = Column(String(30), primary_key=True)  # , default=myuuid()
+    user_email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    user_name = Column(String(20), nullable=False)
+    phone = Column(String(20))
+    birth = Column(String(20))
+    address = Column(String(100))
+    job = Column(String(20))
+    user_interests = Column(String(100))
+    token = Column(String(256))
+
+    articles = relationship('Article', back_populates='user')
+
+    class Config:
+        arbitrary_types_allowed = True
+
+    def __str__(self):
+        return f'아이디: {self.user_id}, \n ' \
+               f'이메일: {self.user_email}, \n ' \
+               f'비밀번호 {self.password} \n ' \
+               f'이름: {self.user_name} \n' \
+               f'전화번호: {self.phone} \n' \
+               f'생년월일: {self.birth} \n' \
+               f'주소: {self.address} \n' \
+               f'직업: {self.job} \n' \
+               f'관심사: {self.user_interests} \n' \
+               f'토큰: {self.token} \n' \
+               f'작성일: {self.created_at} \n' \
+               f'수정일: {self.updated_at}'
